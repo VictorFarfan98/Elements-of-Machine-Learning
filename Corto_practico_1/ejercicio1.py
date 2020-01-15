767dfc65b2217ec7c57b6c2889d0429e406927f9
VictorFarfan98 <42054269+VictorFarfan98@users.noreply.github.com>
2020-01-15 16:51:01 -0600

    Ejercicio 1

---
+++ Corto_practico_1/ejercicio1.py
@@ -0,0 +1,28 @@
+def reverse(number):
+    return int(''.join(reversed(str(number))))
+
+
+def is_palindrome(number):
+    return int(''.join(reversed(str(number)))) == number
+
+
+def is_lychrel(number):
+    attempts = 0
+    while not is_palindrome(number) or attempts > 50:
+        number += reverse(number)
+        attempts += 1
+        if is_palindrome(number):
+            return False
+    if is_palindrome(number):
+        return False
+    else:
+        return True
+
+
+cont = 0
+# for i in range(10000):
+#    if is_lychrel(i):
+#        cont += 1
+
+# print(cont)
+print(is_lychrel(196))
